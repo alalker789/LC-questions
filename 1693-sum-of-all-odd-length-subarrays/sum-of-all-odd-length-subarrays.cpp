3fda111588c163cb6ba2af30d7ccf4a8b6f74d9a
Mansoor Alam <alalker789@gmail.com>
2026-02-11 19:44:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (54.58%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -1,23 +1,41 @@
+// class Solution {
+// public:
+//     int sumArr(vector<int>& arr, int start, int end){
+//         int sum = 0;
+//         for(int i=start ; i<=end ; i++){
+//             sum += arr[i];
+//         }
+//         return sum;
+//     }
+//     int sumOddLengthSubarrays(vector<int>& arr) {
+//         int totalSum = 0;
+//         int n = arr.size();
+
+//         for(int i=0 ; i<n ; i++){
+//             for(int j=i ; j<n ; j++){
+//                 if((j-i+1)%2 != 0){
+//                     totalSum += sumArr(arr, i , j);
+//                 }
+//             }
+//         }
+//         return totalSum;
+//     }
+// };
 class Solution {
 public:
-    int sumArr(vector<int>& arr, int start, int end){
-        int sum = 0;
-        for(int i=start ; i<=end ; i++){
-            sum += arr[i];
-        }
-        return sum;
-    }
     int sumOddLengthSubarrays(vector<int>& arr) {
-        int totalSum = 0;
-        int n = arr.size();
+       int totalSum = 0;
+        for(int i=0; i<arr.size(); i++){
+            int subArrSum = 0;
+            for(int j=i; j<arr.size(); j++){
+                subArrSum += arr[j];
 
-        for(int i=0 ; i<n ; i++){
-            for(int j=i ; j<n ; j++){
-                if((j-i+1)%2 != 0){
-                    totalSum += sumArr(arr, i , j);
+                int length = j-i+1;
+                if(length %2 != 0){
+                    totalSum += subArrSum;
                 }
             }
-        }
+        } 
         return totalSum;
     }
 };
