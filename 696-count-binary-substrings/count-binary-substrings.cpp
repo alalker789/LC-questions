7cb00be387a6a135a7c8c480406f3991c50a870c
Mansoor Alam <alalker789@gmail.com>
2026-02-20 00:22:16 +0530

    Time: 1 ms (75.38%) | Memory: 13 MB (61.91%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int n=s.size();
-        int cnt0=0;
-        int cnt1=0;
-        int totalcnt=0;
-        if(s[0]=='0') cnt0++;
-        else cnt1++;
-        for(int i=1;i<n;i++){
-            if(s[i-1]==s[i]&&s[i]=='0')cnt0++;
-            if(s[i-1]==s[i]&&s[i]=='1')cnt1++;
-            if(s[i-1]!=s[i]&&s[i]=='0'){
-                // now new zero cnt we check if prev1's had something common with prev cnt0
-                 
-                 totalcnt+=min(cnt1,cnt0);// min of both 
-                 cnt0=1;// redoing cntofzeros then
+        int ans = 0, previous = 0, current = 1;
+        for(int i = 1 ; i < s.size() ; i++){
+            if(s[i] == s[i-1]) current++;
+            else{
+                previous = current;
+                current = 1;
             }
-            if(s[i-1]!=s[i]&&s[i]=='1'){
-                 
-                 totalcnt+=min(cnt1,cnt0);
-                 cnt1=1;
-            }
-            
+            if(current <= previous) ans++;
         }
-        totalcnt+=min(cnt1,cnt0);
-        return totalcnt;
+        return ans;
     }
 };
