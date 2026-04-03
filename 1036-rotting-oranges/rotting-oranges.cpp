2ce3b49bb8bb497a120428b9fe9720fab782da86
Mansoor Alam <alalker789@gmail.com>
2026-04-04 01:45:45 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (53.29%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,63 +1,48 @@
 class Solution {
 public:
+    vector<int>delrow = {-1,0,1,0};
+    vector<int>delcol = {0,1,0,-1};
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<pair<pair<int, int>, int>> q;
-        int vis[n][m];
-
-        int fresh = 0;
-
-        // Step 1: Initialize
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<m ; j++){
-                if(grid[i][j] == 2){
-                    q.push({{i,j}, 0});
-                    vis[i][j] = 2;
-                }
-                else if(grid[i][j] == 1){
-                    fresh++;
-                    vis[i][j] = 0;
-                }
-                else{
-                    vis[i][j] = 0;
-                }
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2)
+                q.push({{i,j},0});
             }
         }
+        int tm =0;
 
-        int tm = 0;
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-
-        // Step 2: BFS
         while(!q.empty()){
-            auto it = q.front(); q.pop();
+            
+                auto it = q.front();
+                q.pop();
+                int r = it.first.first;
+                int c = it.first.second;
+                int time = it.second;
+                tm = max(tm,time);
 
-            int r = it.first.first;
-            int c = it.first.second;
-            int t = it.second;
+                for(int i=0;i<4;i++){
+                    int nrow =r+delrow[i];
+                    int ncol = c+ delcol[i];
 
-            tm = max(tm, t);
-
-            for(int i=0 ; i<4 ; i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
-                   && vis[nrow][ncol] != 2 
-                   && grid[nrow][ncol] == 1){
-
-                    q.push({{nrow, ncol}, t+1});
-                    vis[nrow][ncol] = 2;
-                    fresh--;  // orange got rotten
+                    if(nrow>=0 && nrow<n &&ncol>=0 &&ncol< m && grid[nrow][ncol]==1){
+                        q.push({{nrow,ncol},time+1});
+                        grid[nrow][ncol]=2;
+                    }
                 }
-            }
+            
         }
 
-        // Step 3: Check
-        if(fresh > 0) return -1;
-
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] ==1)
+                return -1;
+            }
+        }
         return tm;
+        
     }
 };
