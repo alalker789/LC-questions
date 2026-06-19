ee029914237621596176751f4fd689c8a9bf9af1
Mansoor Alam <alalker789@gmail.com>
2026-06-19 22:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (32.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string s1 = "";
+        string s1;
         for(char ch : s){
             if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9')){
                 s1.push_back(ch);
             }
         }
 
-        int i = 0, j = s1.length()-1;
-        
+        int i=0, j=s1.size()-1;
         while(i<j){
             if(tolower(s1[i]) != tolower(s1[j])) return false;
             i++;
