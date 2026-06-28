64a5d73b06f97591634f8b66cd22a03d29ea1436
Mansoor Alam <alalker789@gmail.com>
2026-06-28 19:46:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (75.14%) - LeetSync

--- 4200-reverse-letters-then-special-characters-in-a-string/reverse-letters-then-special-characters-in-a-string.cpp
+++ 4200-reverse-letters-then-special-characters-in-a-string/reverse-letters-then-special-characters-in-a-string.cpp
@@ -1,53 +1,37 @@
 class Solution {
 public:
-    bool isAlp(char ch) {
+    bool isAlph(char ch){
+        ch = tolower(ch);
         return ch >= 'a' && ch <= 'z';
     }
 
+    void solveAlp(string &s, int left, int right){
+        while(left < right){
+            if(isAlph(s[left]) && isAlph(s[right])){
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+            else if(!isAlph(s[left])) left++;
+            else right--;
+        }
+    }
+
+    void solveNAlp(string &s, int left, int right){
+        while(left < right){
+            if(!isAlph(s[left]) && !isAlph(s[right])){
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+            else if(isAlph(s[left])) left++;
+            else right--;
+        }
+    }
+
     string reverseByType(string s) {
-        int i = 0, j = s.size() - 1;
-
-        // Reverse letters
-        while (i < j) {
-            if (isAlp(s[i]) && isAlp(s[j])) {
-                swap(s[i], s[j]);
-                i++;
-                j--;
-            }
-            else if (!isAlp(s[i]) && !isAlp(s[j])) {
-                i++;
-                j--;
-            }
-            else if (!isAlp(s[i])) {
-                i++;
-            }
-            else {
-                j--;
-            }
-        }
-
-        i = 0;
-        j = s.size() - 1;
-
-        // Reverse special characters
-        while (i < j) {
-            if (!isAlp(s[i]) && !isAlp(s[j])) {
-                swap(s[i], s[j]);
-                i++;
-                j--;
-            }
-            else if (isAlp(s[i]) && isAlp(s[j])) {
-                i++;
-                j--;
-            }
-            else if (isAlp(s[i])) {
-                i++;
-            }
-            else {
-                j--;
-            }
-        }
-
+        solveAlp(s, 0, s.size() - 1);
+        solveNAlp(s, 0, s.size() - 1);
         return s;
     }
 };
