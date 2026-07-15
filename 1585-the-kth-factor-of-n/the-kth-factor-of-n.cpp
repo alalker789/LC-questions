2e52a7e98f1865a0248c3fe26bf0b049329a1783
Mansoor Alam <alalker789@gmail.com>
2026-07-15 22:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (7.00%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> ans;
+
+        for(int i=1 ; i*i<=n ; i++){
+            if(n%i==0){
+                ans.push_back(i);
+                if(n/i != i){
+                    ans.push_back(n/i);
+                }
+            }
+
+        }
+        sort(ans.begin(), ans.end());
+        if(ans.size()>=k) return ans[k-1];
+        return -1;
+    }
+};
