9ef60065d95c102f976f6d5ca5141958b191da12
Mansoor Alam <alalker789@gmail.com>
2026-08-04 19:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (85.37%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -6,19 +6,19 @@ public:
             return;
         }
 
-        for(int i = idx ; i<candidates.size() ; i++){
-            if(i>idx && candidates[i]==candidates[i-1]) continue;
+        for(int i=idx ; i<candidates.size() ; i++){
             if(candidates[i]>target) break;
+            if(i>idx && candidates[i] ==candidates[i-1]) continue;
             temp.push_back(candidates[i]);
             solve(i+1, candidates, target-candidates[i], temp, ans);
             temp.pop_back();
         }
-
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector<vector<int>> ans;
         vector<int> temp;
+        vector<vector<int>> ans;
+        sort(candidates.begin(), candidates.end());
+
         solve(0, candidates, target, temp, ans);
         return ans;
     }
