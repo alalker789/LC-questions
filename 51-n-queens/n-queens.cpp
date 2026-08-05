97832c5a317595708c53ba765fff8b1cd0741512
Mansoor Alam <alalker789@gmail.com>
2026-08-05 09:51:08 +0530

    Time: 3 ms (61.43%) | Memory: 10.1 MB (79.20%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
-    void solve(int col, vector<string>& board, vector<vector<string>>& ans, vector<int>& leftRow, vector<int>& upperDiag, vector<int>& lowerDiag, int n){
+    bool isSafe(int row, int col, vector<string>& board, int n){
+        int dupRow = row;
+        int dupCol = col;
+
+        //check for upper left diagonal
+        while(row>=0 && col>=0){
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        row = dupRow;
+        col = dupCol;
+        //check for left side
+        while(col>=0){
+            if(board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        row = dupRow;
+        col = dupCol;
+        //check for lower left diagonal
+        while(row<n && col>=0){
+            if(board[row][col] == 'Q') return false;
+            row++;
+            col--;
+        }
+        return true;
+    }
+    void solve(int col, vector<string>& board, vector<vector<string>>& ans, int n){
         if(col == n){
             ans.push_back(board);
             return;
         }
 
         for(int row=0 ; row<n ; row++){
-            if(leftRow[row] == 0 && upperDiag[n-1+col-row]== 0 && lowerDiag[row+col] == 0){
+            if(isSafe(row,col, board, n)){
                 board[row][col] = 'Q';
-                leftRow[row] = 1;
-                upperDiag[n-1+col-row] = 1;
-                lowerDiag[row+col] = 1;
-                
-                solve(col+1, board, ans, leftRow, upperDiag, lowerDiag, n);
-
+                solve(col+1, board, ans, n);
                 board[row][col] = '.';
-                leftRow[row] = 0;
-                upperDiag[n-1+col-row] = 0;
-                lowerDiag[row+col] = 0;
             }
         }
     }
@@ -26,10 +47,11 @@ public:
         vector<vector<string>> ans;
         vector<string> board(n);
         string s(n, '.');
-        for(int i=0 ; i<n ; i++) board[i] = s;
 
-        vector<int> leftRow(n, 0), upperDiag(2*n-1, 0), lowerDiag(2*n-1, 0);
-        solve(0, board, ans, leftRow, upperDiag, lowerDiag, n);
+        for(int i=0 ; i<n ; i++){
+            board[i] = s;
+        }
+        solve(0, board, ans, n);
         return ans;
     }
 };
