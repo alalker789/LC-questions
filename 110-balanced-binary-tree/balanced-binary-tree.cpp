1fb8292d9915f4e06e75225b0137142d91f885cf
Mansoor Alam <alalker789@gmail.com>
2026-08-14 20:51:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (21.08%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,42 +11,20 @@
  */
 class Solution {
 public:
-    // brute force
-    // int heightTree(TreeNode* root){
-    //     if(root == NULL) return 0;
-
-    //     return 1 + max(heightTree(root->left), heightTree(root->right)); 
-    // }
-    // bool isBalanced(TreeNode* root) {
-    //     if(root == NULL) return true;
-    //     int lh = heightTree(root->left);
-    //     int rh = heightTree(root->right);
-
-    //     if(abs(rh-lh)>1) return false;
-
-    //     bool left = isBalanced(root->left);
-    //     bool right = isBalanced(root->right);
-
-    //     if(!left || !right) return false;
-
-    //     return true;
-    // }
-
-    //optimised
-    int dfsHeight(TreeNode* root){
+    int height(TreeNode* root){
         if(root == NULL) return 0;
-        int lH = dfsHeight(root->left);
-        if(lH==-1) return -1;
-        
-        int rH = dfsHeight(root->right);
-        if(rH==-1) return -1;
-
-        if(abs(lH-rH)>1) return -1;
-        return 1+max(lH, rH);
+        return 1 + max(height(root->left), height(root->right));
     }
-    
-    bool isBalanced(TreeNode* root){
-        return dfsHeight(root) != -1;
-    }
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL) return true;
+        int lh = height(root->left);
+        int rh = height(root->right);
+        if(abs(lh-rh)>1) return false;
 
+        bool left = isBalanced(root->left);
+        bool right = isBalanced(root->right);
+
+        if(!left || !right) return false;
+        return true;
+    }
 };
