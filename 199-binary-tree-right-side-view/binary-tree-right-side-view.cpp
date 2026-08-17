8ad69d101cf0d67251962111496a91948ef58d19
Mansoor Alam <alalker789@gmail.com>
2026-08-17 11:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (85.74%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,15 +11,16 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* node, int level, vector<int>& ans){
+    vector<int> ans;
+    void solve(TreeNode* node, int level){
         if(node == nullptr) return;
         if(level == ans.size()) ans.push_back(node->val);
-        solve(node->right, level+1, ans);
-        solve(node->left, level+1, ans);
+        solve(node->right, level+1);
+        solve(node->left, level+1);
     }
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        solve(root, 0, ans);
+        
+        solve(root, 0);
         return ans;
     }
 };
